99e7748bebce72f1aa5b386b168aff858286bae3
akshatssrivv <akshatssri@gmail.com>
2025-08-27 09:48:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -435,8 +435,8 @@ with tab2:
             else:
                 column_config[col] = st.column_config.TextColumn(label, help=HELP_TEXTS.get(col))
         
-    # Show the table
-    st.dataframe(display_df, column_config=column_config)
+        # Show the table
+        st.dataframe(display_df, column_config=column_config)
 
             
 
@@ -770,6 +770,7 @@ with tab1:
                 # Display charts
                 st.plotly_chart(fig_residuals, use_container_width=True)
                 st.plotly_chart(fig_velocity, use_container_width=True)
+
 
 
 
