d278fb222436fc4ee0a707116ff4bd437a8c6c99
akshatssrivv <akshatssri@gmail.com>
2025-08-27 12:24:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import requests
 import zipfile
 
 
-SUPABASE_URL = "https://lpxiwnvxqozkjlgfrbfh.supabase.co/storage/v1/object/public/celestial-signal/ns_curves.zip"
+SUPABASE_URL = "https://lpxiwnvxqozkjlgfrbfh.supabase.co/storage/v1/object/public/celestial-signal/ns_curves2708.zip"
 LOCAL_ZIP = "ns_curves_20250822.zip"
 LOCAL_FOLDER = "ns_curves"
 
@@ -771,6 +771,7 @@ with tab1:
                 # Display charts
                 st.plotly_chart(fig_residuals, use_container_width=True)
                 st.plotly_chart(fig_velocity, use_container_width=True)
+
 
 
 
