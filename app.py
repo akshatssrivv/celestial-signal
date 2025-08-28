6465514753f8d203c9581e6e5d434315e8784119
akshatssrivv <akshatssri@gmail.com>
2025-08-28 14:17:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ import boto3
 
 import boto3
 
-B2_KEY_ID = "YOUR_KEY_ID"
-B2_APP_KEY = "YOUR_APP_KEY"
+B2_KEY_ID = "00322044b88660d0000000002"
+B2_APP_KEY = "K003xQHrNmMLe1XF4hKwlbnWZiFlcUA"
 BUCKET_NAME = "Celestial-Signal"
 FILE_KEY = "ns_curves2808.zip"
 
@@ -943,6 +943,7 @@ with tab1:
                 # Display charts
                 st.plotly_chart(fig_residuals, use_container_width=True)
                 st.plotly_chart(fig_velocity, use_container_width=True)
+
 
 
 
